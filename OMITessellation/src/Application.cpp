56430838922d722edbeba7cb0243a30574784bc8
clement-welsch <welsch.clement@gmail.com>
2024-06-20 17:32:25 +0200

    fix issue, we do have only 4 vertex store in the array anymore

--- OMITessellation/src/Application.cpp
+++ OMITessellation/src/Application.cpp
@@ -396,7 +396,7 @@ int main(void)
 	GLuint buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	// define vertex layout
 	glEnableVertexAttribArray(0);
