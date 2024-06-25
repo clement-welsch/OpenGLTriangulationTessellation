0b0b622fbaaaac171adddb2665fa277c757a3469
clement-welsch <welsch.clement@gmail.com>
2024-06-25 12:41:46 +0200

    add VertexBufferLayout and VertexArray classes

---
+++ OMITessellation/include/VertexBufferLayout.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <windows.h>
+#include <vector>
+#include "Renderer.h"
+#include <GL/GL.h>
+
+struct VertexBufferElement
+{
+	unsigned int type;
+	unsigned int count;
+	unsigned char normalized;
+
+	VertexBufferElement(unsigned int _type, unsigned int _count, bool _normalized) : type(_type), count(_count), normalized(_normalized)
+	{}
+
+	static unsigned int GetSizeOfType(unsigned int _type)
+	{
+		switch (_type)
+		{
+			case GL_FLOAT: return 4;
+			case GL_UNSIGNED_INT: return 4;
+			case GL_UNSIGNED_BYTE: return 1;
+			default: ASSERT(false); return 0;
+		}
+	}
+};
+
+class VertexBufferLayout
+{
+public:
+	VertexBufferLayout();
+	~VertexBufferLayout();
+
+	template <typename T>
+	void Push(unsigned int _count)
+	{
+		ASSERT(false);
+	}
+
+	template<>
+	void Push<float>(unsigned int _count)
+	{
+		m_elements.push_back({ GL_FLOAT, _count, GL_FALSE });
+		m_stride += VertexBufferElement::GetSizeOfType(GL_FLOAT) * _count;
+	}
+
+	template<>
+	void Push<unsigned int>(unsigned int _count)
+	{
+		m_elements.push_back({ GL_UNSIGNED_INT, _count, GL_FALSE });
+		m_stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT) * _count;
+	}
+
+	template<>
+	void Push<unsigned char>(unsigned int _count)
+	{
+		m_elements.push_back({ GL_UNSIGNED_BYTE, _count, GL_TRUE });
+		m_stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE) * _count;
+	}
+
+	inline unsigned int GetStride() const { return m_stride; }
+	inline std::vector<VertexBufferElement> GetElements() const { return m_elements; }
+
+private:
+	std::vector<VertexBufferElement> m_elements;
+	unsigned int m_stride;
+
+};
---
+++ OMITessellation/src/VertexArray.cpp
@@ -0,0 +1,39 @@
+#include "VertexArray.h"
+#include "Renderer.h"
+
+
+VertexArray::VertexArray()
+{
+	GLCall(glGenVertexArrays(1, &m_rendererID));
+}
+
+VertexArray::~VertexArray()
+{
+	GLCall(glDeleteVertexArrays(1, &m_rendererID));
+}
+
+void VertexArray::AddBuffer(const VertexBuffer* _vb, const VertexBufferLayout& layout)
+{
+	Bind();
+	_vb->Bind();
+	const auto& elements = layout.GetElements();
+	unsigned int offest = 0;
+
+	for (unsigned int i = 0; i < elements.size(); i++)
+	{
+		const auto& element = elements[i];
+		GLCall(glEnableVertexAttribArray(0));
+		GLCall(glVertexAttribPointer(i, element.count, element.type, element.normalized, layout.GetStride(), (const void*) offest));
+		offest += element.count * VertexBufferElement::GetSizeOfType(element.type);
+	}
+}
+
+void VertexArray::Bind() const
+{
+	GLCall(glBindVertexArray(m_rendererID));
+}
+
+void VertexArray::Unbind() const
+{
+	GLCall(glBindVertexArray(0));
+}
