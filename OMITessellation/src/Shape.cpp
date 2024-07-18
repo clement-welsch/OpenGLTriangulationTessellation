37d3f4d3eb331a7b95ade52df8c6752ae45745bd
clement-welsch <welsch.clement@gmail.com>
2024-07-18 15:53:47 +0200

    add ear clipping triangulation

--- OMITessellation/src/Shape.cpp
+++ OMITessellation/src/Shape.cpp
@@ -1,10 +1,18 @@
 #include "Shape.h"
 #include <fstream>
 #include <sstream>
+#include <earcut.hpp>
+#include <array>
 
 Shape::Shape(const std::string& _filePath)
 {
-	
+	//Ear clipping triangulation
+	using Coord = double;
+	using N = uint32_t;
+	using Point = std::array<Coord, 2>;
+	std::vector<std::vector<std::array<Coord, 2>>> polygon;
+
+
 	std::ifstream infile(_filePath, std::ifstream::binary);
 	std::string line;
 
@@ -15,11 +23,14 @@ Shape::Shape(const std::string& _filePath)
 		if (iss >> value)
 		{
 			m_listVertex.push_back(value);
+			const unsigned size = m_listVertex.size();
 
-			if (m_listVertex.size() % 2 == 0)
+			if (size % 2 == 0)
 			{
-				m_listIndex.push_back(m_listIndex.size());
+				polygon.push_back({ {m_listVertex[size - 2], m_listVertex[size - 1]} });
 			}
 		}
 	}
+	std::vector<N> indices = mapbox::earcut<N>(polygon);
+	m_listIndex = std::copy(vect1.begin(), vect1.end(), back_inserter(vect2));
 }
