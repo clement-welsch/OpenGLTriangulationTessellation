ae2e7c257ee3f37083147c39da7da4030e7b8544
clement-welsch <welsch.clement@gmail.com>
2024-07-18 15:24:52 +0200

    delete delaunay triangulation

--- OMITessellation/include/Shape.h
+++ OMITessellation/include/Shape.h
@@ -1,22 +1,6 @@
 #pragma once
 #include <vector>
 #include <string>
-#include "delaunay.hpp"
-
-struct Patch
-{
-	Patch() {}
-	void AddIndex(int _newIndex)
-	{
-		if (std::find(m_listIndex.begin(), m_listIndex.end(), _newIndex) == m_listIndex.end())
-		{
-			m_listIndex.push_back(_newIndex);
-			std::sort(m_listIndex.begin(), m_listIndex.end());
-		}
-	}
-
-	std::vector<int> m_listIndex;
-};
 
 struct Shape
 {
@@ -25,8 +9,4 @@ struct Shape
 
 	std::vector<float> m_listVertex;
 	std::vector<int> m_listIndex;
-
-private:
-	const unsigned int GetIndex(std::vector<delaunay::Point<float>>& _listDelaunayPoints, delaunay::Point<float> _p);
-	void DelaunayTriangulation();
 };
